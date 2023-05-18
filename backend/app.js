757aebbf5bd5321187d9bde7b9ae135b1c7a3e62
Gunerf <schwerdtlenicolas@gmail.com>
2023-05-18 11:31:48 +0200

    chargement et modification dune image

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
@@ -25,5 +26,6 @@ app.use((req, res, next) => {
 
 app.use('/api/stuff', stuffRoutes)
 app.use('/api/auth', userRoutes)
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 module.exports = app
